673efb9a4dabd2297d79070159e18ce8b2805d13
1yohanyo1 <yohangim119@gmail.com>
2025-05-21 14:21:16 +0900

    challenge 3

--- codingtest/CodingTestChallenge_12/3.py
+++ codingtest/CodingTestChallenge_12/3.py
@@ -1,1 +1,11 @@
-print("Hello World!!")
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+string = [input().strip() for _ in range(n)]
+
+sort_string_set = sorted(set(string)) # 중복 제거
+sort_string = sorted(sort_string_set, key=len) # 길이 정렬
+
+for i in range(len(sort_string)):
+    print(sort_string[i])
